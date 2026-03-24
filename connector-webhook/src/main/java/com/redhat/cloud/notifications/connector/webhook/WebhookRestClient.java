f241d3ee73c13ca8545c844d4fc0a19c4d7d7ec8
Guillaume Duval <gduval@redhat.com>
2026-03-24 11:29:32 +0100

    Add retry logic

--- connector-webhook/src/main/java/com/redhat/cloud/notifications/connector/webhook/WebhookRestClient.java
+++ connector-webhook/src/main/java/com/redhat/cloud/notifications/connector/webhook/WebhookRestClient.java
@@ -5,12 +5,15 @@ import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.HeaderParam;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import java.time.temporal.ChronoUnit;
 
 import static com.redhat.cloud.notifications.connector.webhook.WebhookMessageHandler.JSON_UTF8;
 import static com.redhat.cloud.notifications.connector.webhook.WebhookMessageHandler.X_INSIGHT_TOKEN_HEADER;
 
 @RegisterRestClient(configKey = "connector-rest-client")
+@Retry(delay = 1, delayUnit = ChronoUnit.SECONDS, maxRetries = 2) // 1 initial + 2 retries = 3 attempts
 public interface WebhookRestClient {
 
 
