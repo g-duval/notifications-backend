e1fce712b2b11681c4e5d5a43ae182b8c4d578ed
Guillaume Duval <gduval@redhat.com>
2026-04-17 13:12:25 +0200

    [RHCLOUD-44981] Migrate ServiceNow connector from Camel to Resteasy

---
+++ connector-servicenow/src/main/java/com/redhat/cloud/notifications/connector/servicenow/ServiceNowNotification.java
@@ -0,0 +1,19 @@
+package com.redhat.cloud.notifications.connector.servicenow;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.redhat.cloud.notifications.connector.v2.models.NotificationToConnector;
+import io.quarkus.runtime.annotations.RegisterForReflection;
+import io.vertx.core.json.JsonObject;
+import jakarta.validation.constraints.NotNull;
+
+@RegisterForReflection
+public class ServiceNowNotification extends NotificationToConnector {
+
+    @NotNull
+    @JsonProperty("account_id")
+    public String accountId;
+
+    @NotNull
+    @JsonProperty("notif-metadata")
+    public JsonObject metadata;
+}
